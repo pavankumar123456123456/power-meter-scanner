576372a65e99eb51525c9696133b35cb13fcfb12
pavan.bing <pavan.kumar@taskmo.com>
2023-11-07 23:49:33 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<div>hello</div>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
