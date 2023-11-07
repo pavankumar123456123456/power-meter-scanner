dbd868458c94eb2c993d924e3c98615f7c463dca
pavan.bing <pavan.kumar@taskmo.com>
2023-11-07 23:24:31 +0530

    added login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
-  return <>testing</>;
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
